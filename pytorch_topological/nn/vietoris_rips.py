ba56263632c6bd4fb4e4a90e56d4a28c85ad3bf7
Bastian Rieck <bastian@rieck.me>
2021-12-06 12:26:19 +0100

    Simplified model space loss function

--- pytorch_topological/nn/vietoris_rips.py
+++ pytorch_topological/nn/vietoris_rips.py
@@ -1,36 +1,9 @@
 """Minimise functions based on Vietoris--Rips complexes."""
 
-import numpy as np
-
 from gph import ripser_parallel
 from torch import nn
 
 import torch
-
-
-def _total_persistence(D, p=2):
-    """Calculate total persistence of a persistence diagram.
-
-    Parameters
-    ----------
-    D : `np.array`
-        Persistence diagram, assumed to be in the usual `giotto-ph`
-        format: each entry is supposed to be a tuple of the form $(x, y,
-        d)$, with $(x, y)$ being the usual creation--destruction pair,
-        and $d$ denoting the dimension.
-
-    p : float
-        Weight parameter for the total persistence calculation.
-
-    Returns
-    -------
-    Total persistence of `D`.
-    """
-    persistence = np.diff(D[:, 0:2])
-    persistence = persistence[np.isfinite(persistence)]
-
-    # TODO: Normalise?
-    return np.sum(np.power(np.abs(persistence), p))
 
 
 class ModelSpaceLoss(nn.Module):
@@ -80,7 +53,6 @@ class ModelSpaceLoss(nn.Module):
         Loss
         """
         X = self.X
-        pd_target = self.pd_target
 
         generators = ripser_parallel(
             X.detach(),
