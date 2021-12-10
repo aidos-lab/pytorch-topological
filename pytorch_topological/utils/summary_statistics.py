3dedc07c7d859ecc2a23b77c75e6752283925442
Bastian Rieck <bastian@rieck.me>
2021-12-10 13:22:18 +0100

    Implemented persistent entropy summary statistic

--- pytorch_topological/utils/summary_statistics.py
+++ pytorch_topological/utils/summary_statistics.py
@@ -29,3 +29,36 @@ def total_persistence(D, p=2):
     persistence = persistence[torch.isfinite(persistence)]
 
     return persistence.abs().pow(p).sum()
+
+
+def persistent_entropy(D):
+    """Calculate persistent entropy of a persistence diagram.
+
+    Parameters
+    ----------
+    D : `torch.tensor`
+        Persistence diagram, assumed to be in shape `(n, 2)`, where each
+        entry corresponds to a tuple of the form $(x, y)$, with $x$
+        denoting the creation of a topological feature and $y$ denoting
+        its destruction.
+
+    Returns
+    -------
+    Persistent entropy of `D`.
+    """
+    persistence = torch.diff(D)
+    persistence = persistence[torch.isfinite(persistence)].abs()
+
+    P = persistence.sum()
+    probabilities = torch.as_tensor([
+        p / P for p in persistence
+    ])
+
+    # Ensures that a probability of zero will just result in
+    # a logarithm of zero as well. This is required whenever
+    # one deals with entropy calculations.
+    indices = probabilities > 0
+    log_prob = torch.zeros_like(probabilities)
+    log_prob[indices] = torch.log2(probabilities[indices])
+
+    return torch.sum(-probabilities * log_prob)
