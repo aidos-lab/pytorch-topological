8a4a13d60d5b0bf98bdef003bae07bdc8caf1138
Bastian Rieck <bastian@rieck.me>
2021-12-12 10:08:37 +0100

    Project description and summary statistics demo

--- torch_topological/demos/summary_statistics.py
+++ torch_topological/demos/summary_statistics.py
@@ -3,10 +3,10 @@
 
 import logging
 
-import pytorch_topological.utils as utils
 
-from pytorch_topological.nn import SummaryStatisticLoss
-from pytorch_topological.nn import VietorisRips
+from torch_topological.nn import SummaryStatisticLoss
+from torch_topological.nn import VietorisRips
+from torch_topological.utils import make_disk
 
 import torch
 import torch.optim as optim
@@ -18,14 +18,14 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     n = 100
 
-    X = utils.make_disk(r=0.5, R=0.6, n=n)
-    Y = utils.make_disk(r=0.9, R=1.0, n=n)
+    X = make_disk(r=0.5, R=0.6, n=n)
+    Y = make_disk(r=0.9, R=1.0, n=n)
 
     X = torch.nn.Parameter(torch.as_tensor(X), requires_grad=True)
 
     # loss = ModelSpaceLoss(X, Y, loss=SummaryStatisticLoss)
     vr = VietorisRips(X, Y)
-    loss = SummaryStatisticLoss()
+    loss = SummaryStatisticLoss(p=2)
     opt = optim.SGD([X], lr=0.05)
 
     logging.basicConfig(
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 
     losses = []
 
-    for i in range(100):
+    for i in range(200):
         pd_source, pd_target = vr()
 
         l = loss(pd_source, pd_target)
