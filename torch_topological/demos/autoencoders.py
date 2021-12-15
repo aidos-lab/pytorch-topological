d63e823b70c5a11b876e76ba3b019cc39cc35aac
Bastian Rieck <bastian@rieck.me>
2021-12-15 15:19:24 +0100

    Started preparing demo for simple autoencoders

---
+++ torch_topological/demos/autoencoders.py
@@ -0,0 +1,34 @@
+"""Demo for topology-regularised autoencoders."""
+
+import torch
+
+
+class LinearAutoencoder(torch.nn.Module):
+    """Simple linear autoencoder."""
+    def __init__(self, input_dim, latent_dim=2):
+        super().__init__()
+
+        self.input_dim = input_dim
+        self.latent_dim = latent_dim
+
+        self.encoder = torch.nn.Sequential(
+            torch.nn.Linear(self.input_dim, self.latent_dim)
+        )
+
+        self.decoder = torch.nn.Sequential(
+            torch.nn.Linear(self.latent_dim, self.input_dim)
+        )
+
+        self.loss = torch.nn.MSELoss()
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x):
+        z = self.encode(x)
+        x_hat = self.decode(z)
+        reconstruction_error = self.loss(x, x_hat)
+        return reconstruction_error
