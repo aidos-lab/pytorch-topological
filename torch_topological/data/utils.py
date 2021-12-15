28752c6f014b988de235533451622f9bd00851ce
Bastian Rieck <bastian@rieck.me>
2021-12-15 15:31:17 +0100

    Simple utility functions for data generation

---
+++ torch_topological/data/utils.py
@@ -0,0 +1,39 @@
+"""Utility functions for data set generation."""
+
+import numpy as np
+
+def embed(data, ambient=50):
+    """Embed `data` in `ambient` dimensions.
+
+    Parameters
+    ----------
+    data : array-like
+        Input data set
+
+    ambient : int
+        Dimension of embedding space. Must be greater than
+        dimensionality of data.
+
+    Notes
+    -----
+    This function was originally authored by Nathaniel Saul as part of
+    the `tadasets` package. [tadasets]_
+
+    References
+    ----------
+
+    .. [tadasets] https://github.com/scikit-tda/tadasets
+    """
+    n, d = data.shape
+    assert ambient > d
+
+    base = np.zeros((n, ambient))
+    base[:, :d] = data
+
+    # construct a rotation matrix of dimension `ambient`.
+    random_rotation = np.random.random((ambient, ambient))
+    q, r = np.linalg.qr(random_rotation)
+
+    base = np.dot(base, q)
+
+    return base
