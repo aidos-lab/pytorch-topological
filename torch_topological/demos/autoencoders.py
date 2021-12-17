9a3d945fc00a943f20b2ab29c625f35c1f5d3227
Bastian Rieck <bastian@rieck.me>
2021-12-17 18:26:15 +0100

    Prepared data set for autoencoders demo

--- torch_topological/demos/autoencoders.py
+++ torch_topological/demos/autoencoders.py
@@ -5,10 +5,45 @@ import torch.optim as optim
 
 import matplotlib.pyplot as plt
 
+from torch.utils.data import Dataset
+from torch.utils.data import random_split
+
 from torch_topological.data import create_sphere_dataset
 
 from torch_topological.nn import SignatureLoss
 from torch_topological.nn import VietorisRips
+
+
+class SpheresDataset(Dataset):
+    def __init__(
+        self,
+        train=True,
+        n_samples=50,
+        n_spheres=3,
+        r=5,
+        test_fraction=0.1,
+        seed=42
+    ):
+        X, y = create_sphere_dataset(
+                n_samples=n_samples,
+                n_spheres=spheres,
+                r=r,
+                seed=seed)
+
+        test_size = int(test_fraction * len(X))
+        train_size = len(X) - test_size
+
+        X_train, X_test = torch.utils.random_split(X, [train_size, test_size])
+        y_train, y_test = torch.utils.random_split(y, [train_size, test_size])
+
+        self.data = X_train if train else X_test
+        self.labels = y_train if train else y_test
+
+    def __getitem__(self, index):
+        return self.data[index], self.labels[index]
+
+    def __len__(self):
+        return(self.data)
 
 
 class LinearAutoencoder(torch.nn.Module):
