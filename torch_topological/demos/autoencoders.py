19ae00e1dfd7941e4e07eef52a1a998a1a58ca1c
Bastian Rieck <bastian@rieck.me>
2021-12-18 14:48:45 +0100

    More experiments with autoencoders demo

--- torch_topological/demos/autoencoders.py
+++ torch_topological/demos/autoencoders.py
@@ -31,6 +31,8 @@ class SpheresDataset(Dataset):
                 r=r,
                 seed=seed)
 
+        X = torch.as_tensor(X, dtype=torch.float)
+
         test_size = int(test_fraction * len(X))
         train_size = len(X) - test_size
 
@@ -39,6 +41,8 @@ class SpheresDataset(Dataset):
 
         self.data = X_train if train else X_test
         self.labels = y_train if train else y_test
+
+        self.dimension = X.shape[1]
 
     def __getitem__(self, index):
         return self.data[index], self.labels[index]
@@ -101,42 +105,50 @@ class TopologicalAutoencoder(torch.nn.Module):
         return loss
 
 
-
 if __name__ == '__main__':
-    X, y = create_sphere_dataset(n_samples=50, n_spheres=3)
-    X = torch.as_tensor(X, dtype=torch.float)
+    data_set = SpheresDataset()
 
     train_loader = DataLoader(
-        SpheresDataset(),
-        batch_size=64,
+        data_set,
+        batch_size=256,
         shuffle=True,
         drop_last=True
     )
 
-    model = LinearAutoencoder(input_dim=X.shape[1])
+    model = LinearAutoencoder(input_dim=data_set.dimension)
     topo_model = TopologicalAutoencoder(model, lam=0.1)
 
     optimizer = optim.Adam(topo_model.parameters(), lr=0.1)
 
-    for i in range(10):
+    for i in range(2):
         topo_model.train()
 
-        for batch, (X, y) in enumerate(train_loader):
-            print(X)
+        for batch, (x, y) in enumerate(train_loader):
+            loss = topo_model(x)
 
-        loss = topo_model(X)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            print('.')
+
+    data_set = SpheresDataset(train=True)
+
+    test_loader = DataLoader(
+            data_set,
+            shuffle=True,
+            batch_size=len(data_set)
+    )
+
+    X, y = next(iter(test_loader))
 
     Z = model.encode(X).detach().numpy()
 
     plt.scatter(
         Z[:, 0], Z[:, 1],
-        c=y,
+        c=torch.as_tensor(y, dtype=int),
         cmap='Set1',
-        marker='.',
-        alpha=0.9, s=10.0
+        marker='*',
+        alpha=0.9,
     )
     plt.show()
