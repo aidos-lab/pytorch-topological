9ce22820089b5db51ecf872145256582c649fae0
Bastian Rieck <bastian@rieck.me>
2021-12-22 21:12:09 +0100

    Updated GAN demo
    Still not ready for public consumption but it's a nice way to see
    whether the cubical complex class works or not.

--- torch_topological/demos/gan.py
+++ torch_topological/demos/gan.py
@@ -1,50 +1,35 @@
 """Example of topology-based GANs."""
 
 import torch
+import torchvision
 
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
 from torch_topological.data import make_annulus
 
+from torch_topological.nn import Cubical
 from torch_topological.nn import SummaryStatisticLoss
-from torch_topological.nn import VietorisRips
+
+from tqdm import tqdm
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-class AnnulusDataset(Dataset):
-    def __init__(self, n_samples=100, N=100, r=0.75, R=1.0):
-        X = [
-            torch.as_tensor(make_annulus(N, r, R)[0]) for i in range(n_samples)
-        ]
-
-        X = torch.stack(X)
-        X = torch.as_tensor(X, dtype=torch.float)
-
-        self.data = X
-        self.labels = [1] * len(X)
-
-    def __getitem__(self, index):
-        return self.data[index], self.labels[index]
-
-    def __len__(self):
-        return len(self.data)
-
-
 class Generator(torch.nn.Module):
-    def __init__(self, latent_dim=100, shape=(100, 2)):
+    def __init__(self, latent_dim, shape):
         super().__init__()
 
         self.latent_dim = latent_dim
-        self.output_dim = shape[0] * shape[1]
+        self.output_dim = np.prod(shape)
 
         self.shape = shape
 
         def _make_layer(input_dim, output_dim):
             layers = [
                 torch.nn.Linear(input_dim, output_dim),
+                torch.nn.BatchNorm1d(output_dim, 0.8),
                 torch.nn.LeakyReLU(0.2)
             ]
             return layers
@@ -86,11 +71,11 @@ class Discriminator(torch.nn.Module):
         return self.model(x)
 
 
-class AdversarialLoss(torch.nn.Module):
+class TopologicalAdversarialLoss(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.vr = VietorisRips(dim=1)
+        self.cubical = Cubical()
         self.loss = SummaryStatisticLoss('polynomial_function', p=2, q=2)
 
     def forward(self, real, synthetic):
@@ -98,65 +83,94 @@ class AdversarialLoss(torch.nn.Module):
         loss = 0.0
 
         for x, y in zip(real, synthetic):
-            pi_real = self.vr(x)
-            pi_synthetic = self.vr(y)
+            x = x.squeeze()
+            y = y.squeeze()
 
-            loss += self.loss(pi_real, pi_synthetic)
+            pi_real = self.cubical(x)[0]
+            pi_synthetic = self.cubical(y)[0]
+
+            loss += self.loss([pi_real], [pi_synthetic])
 
         return loss
 
 
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = torchvision.transforms.functional.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+
+
 if __name__ == '__main__':
 
-    n_epochs = 200
-    shape = (100, 2)
+    n_epochs = 5
+
+    img_size = 16
+    shape = (1, img_size, img_size)
+    batch_size = 32
     latent_dim = 200
 
-    data_loader = DataLoader(
-        AnnulusDataset(),
+    data_loader = torch.utils.data.DataLoader(
+        torchvision.datasets.MNIST(
+            "./data/MNIST",
+            train=False,
+            download=True,
+            transform=torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.Resize(img_size),
+                    torchvision.transforms.ToTensor(),
+                    torchvision.transforms.Normalize([0.5], [0.5])
+                ]
+            ),
+        ),
+        batch_size=batch_size,
         shuffle=True,
-        batch_size=8,
     )
 
     generator = Generator(shape=shape, latent_dim=latent_dim)
     discriminator = Discriminator(shape=shape)
-    adversarial_loss = torch.nn.BCELoss() # AdversarialLoss()
+    adversarial_loss = torch.nn.BCELoss()
+    topo_loss = TopologicalAdversarialLoss()
 
     opt_g = torch.optim.Adam(generator.parameters(), lr=1e-4)
     opt_d = torch.optim.Adam(discriminator.parameters(), lr=1e-4)
 
     for epoch in range(n_epochs):
-        for batch, (pc_real, _) in enumerate(data_loader):
+        for batch, (imgs, _) in tqdm(enumerate(data_loader), desc='Batch'):
             z = torch.autograd.Variable(
                 torch.Tensor(
                     np.random.normal(
                         0,
                         1,
-                        (pc_real.shape[0], latent_dim)
+                        (imgs.shape[0], latent_dim)
                     )
                 )
             )
 
-            real_labels = torch.as_tensor([1.0] * len(pc_real)).view(-1, 1)
-            fake_labels = torch.as_tensor([0.0] * len(pc_real)).view(-1, 1)
+            real_labels = torch.as_tensor([1.0] * len(imgs)).view(-1, 1)
+            fake_labels = torch.as_tensor([0.0] * len(imgs)).view(-1, 1)
 
             opt_g.zero_grad()
 
-            pc_synthetic = generator(z)
+            imgs_synthetic = generator(z)
 
-            #generator_loss = adversarial_loss(pc_real, pc_synthetic)
             generator_loss = adversarial_loss(
-                discriminator(pc_synthetic), real_labels
-            )
+                discriminator(imgs_synthetic), real_labels
+            ) + 0.01 * topo_loss(imgs, imgs_synthetic)
+
             generator_loss.backward()
 
             opt_g.step()
 
             opt_d.zero_grad()
 
-            real_loss = adversarial_loss(discriminator(pc_real), real_labels)
+            real_loss = adversarial_loss(discriminator(imgs), real_labels)
             fake_loss = adversarial_loss(
-                discriminator(pc_synthetic).detach(), fake_labels
+                discriminator(imgs_synthetic).detach(), fake_labels
             )
 
             discriminator_loss = 0.5 * (real_loss + fake_loss)
@@ -164,10 +178,9 @@ if __name__ == '__main__':
 
             opt_d.step()
 
+    output = imgs_synthetic.detach()
+    grid = torchvision.utils.make_grid(output)
 
-    output = pc_synthetic.detach().numpy()
-
-    for X_ in output:
-        plt.scatter(X_[:, 0], X_[:, 1])
+    show(grid)
 
     plt.show()
