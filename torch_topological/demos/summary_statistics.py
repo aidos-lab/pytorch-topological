463862f4c1f28c7b7d747aae71ce03862544f081
Bastian Rieck <bastian@rieck.me>
2021-12-30 18:04:33 +0100

    Improved summary statistics example
    * Made various parameters configurable
    * Simplified output

--- torch_topological/demos/summary_statistics.py
+++ torch_topological/demos/summary_statistics.py
@@ -4,7 +4,9 @@ This example demonstrates how to use various topological summary
 statistics in order to change the shape of an input point cloud.
 """
 
-import logging
+import argparse
+
+import matplotlib.pyplot as plt
 
 from torch_topological.data import sample_from_disk
 
@@ -16,11 +18,11 @@ from tqdm import tqdm
 import torch
 import torch.optim as optim
 
-import matplotlib.pyplot as plt
 
-
-if __name__ == '__main__':
-    n = 100
+def main(args):
+    """Run example."""
+    n = args.n_samples
+    n_iterations = args.n_iterations
 
     X = sample_from_disk(n=n, r=0.5, R=0.6)
     Y = sample_from_disk(n=n, r=0.9, R=1.0)
@@ -30,30 +32,22 @@ if __name__ == '__main__':
     # loss = ModelSpaceLoss(X, Y, loss=SummaryStatisticLoss)
     vr = VietorisRipsComplex(dim=2)
     pi_target = vr(Y)
-    loss = SummaryStatisticLoss('polynomial_function', p=2, q=2)
+    loss_fn = SummaryStatisticLoss('polynomial_function', p=2, q=2)
     #loss = SummaryStatisticLoss('polynomial_function', p=2, q=2)
     opt = optim.SGD([X], lr=0.05)
 
-    logging.basicConfig(
-        level=logging.INFO
-    )
+    progress = tqdm(range(n_iterations))
 
-    losses = []
-
-    for i in range(500):
+    for i in progress:
         pi_source = vr(X)
 
-        l = loss(pi_source, pi_target)
+        loss = loss_fn(pi_source, pi_target)
 
         opt.zero_grad()
-
-        l.backward()
+        loss.backward()
         opt.step()
 
-        l = l.detach().numpy()
-
-        logging.info(l)
-        losses.append(l)
+        progress.set_postfix(loss=f'{loss.item():.08f}')
 
     X = X.detach().numpy()
 
@@ -63,3 +57,46 @@ if __name__ == '__main__':
     plt.legend()
     plt.show()
 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-i', '--n-iterations',
+        default=250,
+        type=int,
+        help='Number of iterations'
+    )
+
+    parser.add_argument(
+        '-n', '--n-samples',
+        default=100,
+        type=int,
+        help='Number of samples in point clouds'
+    )
+
+    parser.add_argument(
+        '-s', '--statistic',
+        choices=[
+            'persistent_entropy',
+            'polynomial_function',
+            'total_persistence',
+        ],
+        help='Name of summary statistic to use for the loss'
+    )
+
+    parser.add_argument(
+        '-p',
+        type=float,
+        help='Outer exponent for summary statistic loss calculation'
+    )
+
+    parser.add_argument(
+        '-q',
+        type=float,
+        help='Inner exponent for summary statistic loss calculation. Will '
+             'only be used for certain summary statistics.'
+    )
+
+    args = parser.parse_args()
+    main(args)
