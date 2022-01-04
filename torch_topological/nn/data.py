a648e50f7765579575813d1deb5d1a183c365fbe
Bastian Rieck <bastian@rieck.me>
2022-01-04 19:12:43 +0100

    Started implementing conversion methods for persistence info objects

--- torch_topological/nn/data.py
+++ torch_topological/nn/data.py
@@ -2,6 +2,15 @@
 
 from collections import namedtuple
 
+from itertools import chain
+
+from operator import itemgetter
+
+from torch_topological.utils import is_iterable
+from torch_topological.utils import nesting_level
+
+import torch
+
 
 class PersistenceInformation(namedtuple(
             'PersistenceInformation',
@@ -31,3 +40,51 @@ class PersistenceInformation(namedtuple(
     """
 
     __slots__ = ()
+
+
+def make_tensor(x):
+    """Create dense tensor representation from sparse inputs."""
+    level = nesting_level(x)
+
+    # List of lists: the first axis is treated as the batch axis, while
+    # the second axis is treated as the dimension of diagrams or pairs.
+    if level == 2:
+        B = len(x)
+
+        # Collect information that we need to create the full tensor. An
+        # entry of the resulting list contains the length of the diagram
+        # and the dimension, making it possible to derive padding values
+        # for all entries.
+        M = list(map(
+            lambda a: (len(a.diagram), a.dimension), chain.from_iterable(x)
+        ))
+
+        print(list(M))
+
+        N = max(M, key=itemgetter(0))[0]
+        D = max(M, key=itemgetter(1))[1]
+
+        print(B, N, D)
+
+        make_tensor_from_persistence_information(x[0][0])
+
+
+def make_tensor_from_persistence_information(pers_info):
+    """Convert (sequence) of persistence information entries to tensor."""
+    if is_iterable(pers_info) or \
+            isinstance(pers_info[0], PersistenceInformation):
+        pers_info = [pers_info]
+
+    pairs = torch.cat(
+        [torch.as_tensor(x.diagram, dtype=torch.float) for x in pers_info]
+    )
+
+    dimensions = torch.cat(
+        [
+            torch.as_tensor([x.dimension] * len(x.diagram), dtype=torch.float)
+            for x in pers_info
+        ]
+    )
+
+    result = torch.column_stack((pairs, dimensions))
+    return result
