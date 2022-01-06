9f5f59cb626931982408d46d093347b0db505027
Bastian Rieck <bastian@rieck.me>
2022-01-06 15:54:37 +0100

    Basic smoke test for structure element layer

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -3,14 +3,15 @@ from torch_topological.datasets import SphereVsTorus
 from torch_topological.nn.data import make_tensor
 
 from torch_topological.nn import VietorisRipsComplex
+from torch_topological.nn.layers import StructureElementLayer
 
 from torch.utils.data import DataLoader
 
-batch_size = 64
+batch_size = 32
 
 
 class TestStructureElementLayer:
-    data_set = SphereVsTorus(n_point_clouds=batch_size)
+    data_set = SphereVsTorus(n_point_clouds=2 * batch_size)
     loader = DataLoader(
         data_set,
         batch_size=batch_size,
@@ -20,8 +21,12 @@ class TestStructureElementLayer:
 
     vr = VietorisRipsComplex(dim=1)
 
+    layer = StructureElementLayer(10)
+
     def test_processing(self):
         for (x, y) in self.loader:
             pers_info = make_tensor(self.vr(x))
+            output = self.layer(pers_info)
 
             assert pers_info is not None
+            assert output is not None
