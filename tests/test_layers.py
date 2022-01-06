bb21820ca519f2e572479eec94609132401d43ad
Bastian Rieck <bastian@rieck.me>
2022-01-06 15:12:24 +0100

    Prepared test module for layers
    Currently implementing a simple layer based on structure elements;
    I want to have the scaffolding already in place to see what needs
    fixing early on.

---
+++ tests/test_layers.py
@@ -0,0 +1,28 @@
+from torch_topological.datasets import SphereVsTorus
+
+from torch_topological.nn.data import make_tensor
+
+from torch_topological.nn import VietorisRipsComplex
+
+from torch.utils.data import DataLoader
+
+batch_size = 64
+
+
+class TestStructureElementLayer:
+    data_set = SphereVsTorus(n_point_clouds=3 * batch_size)
+    loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=False,
+    )
+
+    vr = VietorisRipsComplex(dim=1)
+
+    def test_processing(self):
+        for (x, y) in self.loader:
+            pers_info = make_tensor(self.vr(x))
+
+            assert pers_info is not None
+            assert len(pers_info) == batch_size
