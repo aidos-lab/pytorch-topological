69c88aa020d3e836a6e9a3d041fc8ba732446f48
Bastian Rieck <bastian@rieck.me>
2022-01-19 08:10:06 +0100

    More unit tests for Wasserstein distance calculations

--- tests/test_ot.py
+++ tests/test_ot.py
@@ -1,5 +1,6 @@
-import torch
+import numpy as np
 
+import torch
 
 from torch_topological.nn import PersistenceInformation
 from torch_topological.nn import WassersteinDistance
@@ -27,6 +28,32 @@ class TestWassersteinDistance:
             (5, 3),
             (7, 4)
         ]
+
+        X = wrap(X)
+        Y = wrap(Y)
+
+        dist = WassersteinDistance()(X, Y)
+        assert dist > 0.0
+
+    def test_random(self):
+        n_points = 10
+        n_instances = 10
+
+        for i in range(n_instances):
+            X = np.random.default_rng().normal(size=(n_points, 2))
+            Y = np.random.default_rng().normal(size=(n_points, 2))
+
+            X = wrap(X)
+            Y = wrap(Y)
+
+            dist = WassersteinDistance()(X, Y)
+            assert dist > 0.0
+
+    def test_almost_zero(self):
+        n_points = 100
+
+        X = np.random.default_rng().uniform(-1e-16, 1e-11, size=(n_points, 2))
+        Y = np.random.default_rng().uniform(-1e-16, 1e-11, size=(n_points, 2))
 
         X = wrap(X)
         Y = wrap(Y)
