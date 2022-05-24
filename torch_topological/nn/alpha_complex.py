e23a6c7637e9a93ec2f4ddaa670b646d308e05ee
Bastian Rieck <bastian@rieck.me>
2022-05-24 13:14:44 +0200

    Working on adding gradient information
    So far, this only works for dimension 0, where there are not a lot of
    obstacles anyway. Looking into higher dimensions requires more effort
    because of the way the complex is defined.

--- torch_topological/nn/alpha_complex.py
+++ torch_topological/nn/alpha_complex.py
@@ -13,6 +13,9 @@ class AlphaComplex(nn.Module):
     def __init__(self):
         super().__init__()
 
+        # TODO: Make configurable
+        self.p = 2
+
     def forward(self, x):
         # TODO: Copied from `VietorisRipsComplex`; needs to be
         # refactored into a unified interface.
@@ -36,24 +39,60 @@ class AlphaComplex(nn.Module):
         )
 
         st = alpha_complex.create_simplex_tree()
-        persistence_points = st.persistence()
+        st.persistence()
+        persistence_pairs = st.persistence_pairs()
 
-        max_dim = x.shape[-1]
+        max_dim = 0  # TODO: Should be x.shape[-1]
+
+        dist = torch.cdist(x, x, p=self.p)
 
         return [
-            self._extract_diagrams(persistence_points, dim)
-            for dim in range(0, max_dim)
+            self._extract_generators_and_diagrams(
+                dist,
+                persistence_pairs,
+                dim
+            )
+            for dim in range(0, max_dim + 1)
         ]
 
-    def _extract_diagrams(self, persistence_points, dim):
-        points = [
-            torch.as_tensor(p) for d, p in persistence_points if d == dim
+    def _extract_generators_and_diagrams(self, dist, persistence_pairs, dim):
+        pairs = [
+            torch.cat((torch.as_tensor(p[0]), torch.as_tensor(p[1])), 0)
+            for p in persistence_pairs if len(p[0]) == (dim + 1)
         ]
 
-        persistence_diagram = torch.stack(points, 0)
+        # TODO: Ignore infinite features for now. Will require different
+        # handling in the future.
+        pairs = [p for p in pairs if len(p) == 2 * dim + 3]
 
-        return PersistenceInformation(
-                pairing=None,
-                diagram=persistence_diagram,
-                dimension=dim
-        )
+        # Create tensor of shape `(n, 2 * dim + 3)`, with `n` being the
+        # number of finite persistence pairs.
+        pairs = torch.stack(pairs)
+
+        if dim == 0:
+            creators = torch.zeros_like(torch.as_tensor(pairs)[:, 0])
+            destroyers = dist[pairs[:, 1], pairs[:, 2]]
+
+            persistence_diagram = torch.stack(
+                (creators, destroyers), 1
+            )
+
+            return PersistenceInformation(
+                    pairing=pairs,
+                    diagram=persistence_diagram,
+                    dimension=0
+            )
+
+        # Iterate over the flag complex in order to get (a) the distance
+        # of the creator simplex, and (b) the distance of the destroyer.
+        # We *cannot* look this information up in the filtration itself,
+        # because we have to use gradient-imbued information such as the
+        # set of pairwise distances.
+        else:
+            pass
+
+        #return PersistenceInformation(
+        #        pairing=None,
+        #        diagram=persistence_diagram,
+        #        dimension=dim
+        #)
