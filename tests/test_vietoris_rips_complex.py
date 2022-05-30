12aa546463a14870ccebbb5cc78fbbf3204de38c
Bastian Rieck <bastian@rieck.me>
2022-05-30 09:29:38 +0200

    Updated test case to handle arbitrary distances

--- tests/test_vietoris_rips_complex.py
+++ tests/test_vietoris_rips_complex.py
@@ -5,12 +5,52 @@ from torch_topological.nn.data import make_tensor
 from torch_topological.nn.data import PersistenceInformation
 
 from torch_topological.nn import VietorisRipsComplex
+from torch_topological.nn import WassersteinDistance
 
+from torch import cdist
 from torch.utils.data import DataLoader
+
+import pytest
 
 import numpy as np
 
 batch_size = 64
+
+
+class TestVietorisRipsComplex:
+    data_set = SphereVsTorus(n_point_clouds=3 * batch_size)
+    loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=True,
+        drop_last=False,
+    )
+
+    vr = VietorisRipsComplex(dim=1, p=1)
+
+    def test_simple(self):
+        for (x, y) in self.loader:
+            pers_info = self.vr(x)
+
+            assert pers_info is not None
+            assert len(pers_info) == batch_size
+
+    def test_predefined_distances(self):
+        for (x, y) in self.loader:
+
+            distances = cdist(x, x, p=1)
+            pers_info1 = self.vr(distances, treat_as_distances=True)
+            pers_info2 = self.vr(x, treat_as_distances=False)
+
+            assert pers_info1 is not None
+            assert pers_info2 is not None
+            assert len(pers_info1) == batch_size
+            assert len(pers_info1) == len(pers_info2)
+
+            # Check that we are getting the same persistence diagrams.
+            for pi1, pi2 in zip(pers_info1, pers_info2):
+                dist = WassersteinDistance()(pi1, pi2)
+                assert dist == pytest.approx(0.0)
 
 
 class TestVietorisRipsComplexBatchHandling:
