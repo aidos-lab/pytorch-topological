1d0ef32e5d036138b59b6352ff8aaef22ea3040a
Bastian Rieck <bastian@rieck.me>
2022-05-30 08:05:44 +0200

    Extended cubical complex test case

--- tests/test_cubical_complex.py
+++ tests/test_cubical_complex.py
@@ -1,7 +1,18 @@
+from torchvision.datasets import MNIST
+
+from torchvision.transforms import Compose
+from torchvision.transforms import Normalize
+from torchvision.transforms import ToTensor
+
 from torch_topological.nn import CubicalComplex
 
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+from torch.utils.data import RandomSampler
+
+from torch_topological.nn.data import batch_iter
+from torch_topological.nn.data import make_tensor
+from torch_topological.nn.data import PersistenceInformation
 
 import numpy as np
 
@@ -104,3 +115,65 @@ class TestCubicalComplex:
 
             assert pers_info is not None
             assert len(pers_info) == self.batch_size
+
+
+class TestCubicalComplexBatchHandling:
+    batch_size = 64
+
+    data_set = MNIST(
+        './data/MNIST',
+        train=False,
+        transform=Compose(
+            [
+                ToTensor(),
+                Normalize([0.5], [0.5])
+            ]
+        ),
+    )
+
+    loader = DataLoader(
+        data_set,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=True,
+        sampler=RandomSampler(
+            data_set,
+            replacement=True,
+            num_samples=100
+        )
+    )
+
+    cc = CubicalComplex()
+
+    def test_processing(self):
+        for (x, y) in self.loader:
+            pers_info = self.cc(x)
+
+            assert pers_info is not None
+            assert len(pers_info) == self.batch_size
+
+            pers_info_dense = make_tensor(pers_info)
+
+            assert pers_info_dense is not None
+
+    def test_batch_iter(self):
+        for (x, y) in self.loader:
+            pers_info = self.cc(x)
+
+            assert pers_info is not None
+            assert len(pers_info) == self.batch_size
+
+            assert sum(1 for x in batch_iter(pers_info)) == self.batch_size
+
+            for x in batch_iter(pers_info):
+                for y in x:
+                    assert isinstance(y, PersistenceInformation)
+
+            for x in batch_iter(pers_info, dim=0):
+
+                # Make sure that we have something to iterate over.
+                assert sum(1 for y in x) != 0
+
+                for y in x:
+                    assert isinstance(y, PersistenceInformation)
+                    assert y.dimension == 0
