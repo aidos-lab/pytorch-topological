2d1abee0f2d7176105a7131798b1d5b583a575dc
Bastian Rieck <bastian@rieck.me>
2022-05-30 07:59:56 +0200

    Added separate test case for Vietoris--Rips complex

--- tests/test_vietoris_rips_complex.py
+++ tests/test_vietoris_rips_complex.py
@@ -1,25 +1,19 @@
-from torchvision.datasets import MNIST
-
-from torchvision.transforms import Compose
-from torchvision.transforms import Normalize
-from torchvision.transforms import ToTensor
-
 from torch_topological.datasets import SphereVsTorus
 
 from torch_topological.nn.data import batch_iter
 from torch_topological.nn.data import make_tensor
 from torch_topological.nn.data import PersistenceInformation
 
-from torch_topological.nn import AlphaComplex
-from torch_topological.nn import CubicalComplex
+from torch_topological.nn import VietorisRipsComplex
 
 from torch.utils.data import DataLoader
-from torch.utils.data import RandomSampler
+
+import numpy as np
 
 batch_size = 64
 
 
-class TestAlphaComplexBatchHandling:
+class TestVietorisRipsComplexBatchHandling:
     data_set = SphereVsTorus(n_point_clouds=3 * batch_size)
     loader = DataLoader(
         data_set,
@@ -28,11 +22,11 @@ class TestAlphaComplexBatchHandling:
         drop_last=False,
     )
 
-    ac = AlphaComplex()
+    vr = VietorisRipsComplex(dim=1)
 
     def test_processing(self):
         for (x, y) in self.loader:
-            pers_info = self.ac(x)
+            pers_info = self.vr(x)
 
             assert pers_info is not None
             assert len(pers_info) == batch_size
@@ -41,51 +35,30 @@ class TestAlphaComplexBatchHandling:
 
             assert pers_info_dense is not None
 
+    def test_ragged_processing(self):
+        rng = np.random.default_rng()
 
-class TestCubicalComplexBatchHandling:
-    data_set = MNIST(
-        './data/MNIST',
-        train=False,
-        transform=Compose(
-            [
-                ToTensor(),
-                Normalize([0.5], [0.5])
-            ]
-        ),
-    )
+        data = [
+            np.random.default_rng().uniform(size=(rng.integers(32, 64), 8))
+            for _ in range(batch_size)
+        ]
 
-    loader = DataLoader(
-        data_set,
-        batch_size=batch_size,
-        shuffle=False,
-        drop_last=True,
-        sampler=RandomSampler(
-            data_set,
-            replacement=True,
-            num_samples=100
-        )
-    )
+        pers_info = self.vr(data)
 
-    cc = CubicalComplex()
-
-    def test_processing(self):
-        for (x, y) in self.loader:
-            pers_info = self.cc(x)
-
-            assert pers_info is not None
-            assert len(pers_info) == batch_size
-
-            pers_info_dense = make_tensor(pers_info)
-
-            assert pers_info_dense is not None
+        assert pers_info is not None
+        assert len(pers_info) == batch_size
 
     def test_batch_iter(self):
         for (x, y) in self.loader:
-            pers_info = self.cc(x)
+            pers_info = self.vr(x)
 
             assert pers_info is not None
             assert len(pers_info) == batch_size
 
+            # This is just to confirm that we can properly iterate over
+            # this batch. Here, `batch_iter` is a little bit like `NoP`,
+            # but in general, more complicated nested structures may be
+            # present.
             assert sum(1 for x in batch_iter(pers_info)) == batch_size
 
             for x in batch_iter(pers_info):
