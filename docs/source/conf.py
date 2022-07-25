c6eed4097a5362cb2dd38cf53a85a4a211b62f25
Bastian Rieck <bastian@rieck.me>
2022-07-25 07:32:49 +0200

    Added logo to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,6 +59,11 @@ exclude_patterns = ['build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = '../../torch_topological.svg'
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
