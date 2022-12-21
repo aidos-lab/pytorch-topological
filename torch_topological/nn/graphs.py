2743812754f2be45659c8fa2ba2bfc15dc6b0791
Bastian Rieck <bastian@rieck.me>
2022-12-21 10:24:57 +0100

    Experimenting with graph conversion
    Need to be able to convert `PyG` graphs to simplicial complexes while
    maintaining gradients. That way, it should be possible to reduce each
    persistent homology calculation.

---
+++ torch_topological/nn/graphs.py
@@ -0,0 +1,53 @@
+"""Layers for topological data analysis based on graphs."""
+
+from torch_geometric.data import Batch
+from torch_geometric.data import Data
+
+from torch_geometric.utils import erdos_renyi_graph
+
+import torch
+
+B = 64
+N = 100
+p = 0.2
+
+X = [
+    Data(x=torch.rand(N, 1), edge_index=erdos_renyi_graph(N, p), num_nodes=N)
+]
+
+X = Batch.from_data_list(X)
+
+vertex_slices = torch.Tensor(X._slice_dict['x']).long()
+edge_slices = torch.Tensor(X._slice_dict['edge_index']).long()
+
+print(vertex_slices)
+print(edge_slices)
+
+
+#def calculate_persistent_homology(G, k=3):
+#    """Calculate persistent homology of graph clique complex."""
+#    st = gd.SimplexTree()
+#
+#    for v, w in G.nodes(data=True):
+#        weight = w["curvature"]
+#        st.insert([v], filtration=weight)
+#
+#    for u, v, w in G.edges(data=True):
+#        weight = w["curvature"]
+#        st.insert([u, v], filtration=weight)
+#
+#    st.make_filtration_non_decreasing()
+#    st.expansion(k)
+#    persistence_pairs = st.persistence()
+#
+#    diagrams = []
+#
+#    for dimension in range(k + 1):
+#        diagram = [
+#            (c, d) for dim, (c, d) in persistence_pairs if dim == dimension
+#        ]
+#
+#        diagrams.append(diagram)
+#
+#    return diagrams
+#
