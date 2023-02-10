6b18b53979f8bbf305cfe9fc4a2e4ae8b167b05e
Bastian Rieck <bastian@rieck.me>
2023-02-10 14:59:04 +0100

    Updated benchmarking script

--- torch_topological/examples/benchmarking.py
+++ torch_topological/examples/benchmarking.py
@@ -8,19 +8,34 @@ from torch_topological.nn import WassersteinDistance
 from torch_topological.nn import VietorisRipsComplex
 
 
-if __name__ == "__main__":
-    X = torch.load(sys.argv[1])
-
-    vr = VietorisRipsComplex(dim=0)
+def run_test(X, vr, name, dist=False):
     W1 = WassersteinDistance()
 
     pre = time.perf_counter()
 
-    X_pi = vr(X)
-    Y_pi = vr(X)
+    X_pi = vr(X, treat_as_distances=dist)
+    Y_pi = vr(X, treat_as_distances=dist)
 
     dists = torch.stack([W1(x_pi, y_pi) for x_pi, y_pi in zip(X_pi, Y_pi)])
     dist = dists.mean()
 
     cur = time.perf_counter()
-    print(f"{cur - pre:.4f}s")
+    print(f"{name}: {cur - pre:.4f}s")
+
+
+if __name__ == "__main__":
+    X = torch.load(sys.argv[1])
+
+    print("Calculating everything ourselves")
+
+    run_test(X, VietorisRipsComplex(dim=0), "raw")
+    run_test(X, VietorisRipsComplex(dim=0, threshold=1.0), "thresholded")
+
+    print("\nPre-defined distances")
+
+    D = torch.cdist(X, X)
+
+    run_test(D, VietorisRipsComplex(dim=0), "raw", dist=True)
+    run_test(
+        D, VietorisRipsComplex(dim=0, threshold=1.0), "thresholded", dist=True
+    )
